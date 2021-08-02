f7a3b52d8447f8a405fcb7ac8b6981ddfbc1403e
Yachu Joshi <yachujoshi1234@gmail.com>
2021-08-02 11:47:08 +0545

    Inital Setup :tada:

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      <div>Hello World!</div>
+    </>
+  );
+};
+
+export default Home;
