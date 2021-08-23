dd05ec0c84e52adb75a789d4adf9d35344c0f739
Yachu Joshi <yachujoshi1234@gmail.com>
2021-08-23 15:54:46 +0545

    Update card to show book-due-date

--- src/books/BooksCard.jsx
+++ src/books/BooksCard.jsx
@@ -1,10 +1,21 @@
+import cx from "classnames";
 import Image from "next/image";
 import styles from "./BooksCard.module.scss";
 
 export const BooksCard = ({ book }) => {
   const num = Math.floor(Math.random() * 50 + 1);
+  const { late_due_days: lateDueDays } = book;
   return (
     <div className={styles.BookCard}>
+      {lateDueDays && (
+        <span
+          className={cx(styles.LateDueDays, {
+            [styles.Late]: lateDueDays > 0,
+          })}
+        >
+          {lateDueDays < 0 ? -lateDueDays : lateDueDays} Days Left
+        </span>
+      )}
       <figure className={styles.BookCardFigure}>
         <Image
           src={`https://unsplash.it/360/200?image=${num}`}
